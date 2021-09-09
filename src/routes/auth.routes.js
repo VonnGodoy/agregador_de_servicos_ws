6695bd65f970d80350f6b7f6acb1216411ec2f45
VonnGodoy <60471992+VonnGodoy@users.noreply.github.com>
2021-09-09 20:38:14 -0300

    ajuste refresh tken erro 401

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -55,7 +55,7 @@ router.post('/refresh', async (req, res) => {
         return res.status(401).send({ error: true, message: 'Usuario Não Autenticado.' });
 
     var publicKey = fs.readFileSync('src/oauth/public.key', 'utf8');
-    jwt.verify(token, publicKey, { algorithm: ["RS256"] }, function (err, decoded) {
+    jwt.decode(token, { algorithm: ["RS256"] }, function (err, decoded) {
         getUserRefresh(decoded.id);
     });
 
