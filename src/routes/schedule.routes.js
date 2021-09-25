14310c6cf0026ce0b7a6efbc3b11b2a0da056731
VonnGodoy <60471992+VonnGodoy@users.noreply.github.com>
2021-09-25 12:16:02 -0300

    erro consulta

--- src/routes/schedule.routes.js
+++ src/routes/schedule.routes.js
@@ -49,7 +49,7 @@ router.post('/filter', async (req, res) => {
 
     const filter = req.body;
 
-    const schedule = await Schedule.aggregate([
+    const schedule = await Schedule.aggregate(
       {
         $geoNear: {
           near: {
@@ -61,7 +61,7 @@ router.post('/filter', async (req, res) => {
           maxDistance: filter.maxDistance * 1000,
         },
       },
-    ])
+    )
     .select('_id name socialReason geoLocation dateRegister state');;
 
     res.json({ error: false, data: schedule });
